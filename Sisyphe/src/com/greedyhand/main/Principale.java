4353901160ceeb7e50218349fd66b62cd25fc9c8
Aleaxandre <alexandre.masurel@gmail.com>
2014-07-09 22:47:39 +0200

    Initial commit

---
+++ Sisyphe/src/com/greedyhand/main/Principale.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.greedyhand.main;
+
+/**
+ * @author A.Masurel
+ *
+ */
+public class Principale {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Project main class");
+	}
+
+}
